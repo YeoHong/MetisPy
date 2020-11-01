08bf6023e558e489065fb308c6e952fc72a1cafc
yhyoon <bubble94@naver.com>
2020-11-01 23:14:03 +0900

    Basic GUI complete.

--- __main__.py
+++ __main__.py
@@ -1,15 +1,49 @@
+import argparse
+import logging
+
 import sys, os
 sys.path.append(os.path.abspath('..')) # To find local version of the library
 
+from PySide2 import QtCore, QtWidgets
+
 from MetisPy import ROOT_DIR
-from MetisPy import __appname__
+from MetisPy import __appname__, __version__
+
+from MetisPy.canvas.app import MainWindow
+from MetisPy.utils.logger import logger 
+
+
 
 def main():
-    print(__appname__)
-    print(ROOT_DIR)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--version", "-V", action="store_true", help="show version")
+    parser.add_argument("--logger-level",
+                        default="info",
+                        choices=["debug", "info", "warning", "fatal", "error"],
+                        help="logger level",
+                        )
+    
+    args = parser.parse_args()
 
+    if args.version:
+        print("{0} {1}".format(__appname__, __version__))
+        sys.exit(0)
+    
+    logger.setLevel(getattr(logging, args.logger_level.upper()))
 
+    logger.info("{0} {1}".format(__appname__, __version__))    
 
+    app = QtWidgets.QApplication(sys.argv)
+    app.setApplicationName(__appname__)
+
+    win = MainWindow()
+    
+    win.show()
+    win.raise_()
+
+    sys.exit(app.exec_())
+
+    
 
 if __name__ == "__main__":
     main()
---
+++ canvas/widgets/unique_label_qlist_widget.py
@@ -0,0 +1,44 @@
+from PySide2 import QtWidgets, QtCore
+
+
+class EscapableQListWidget(QtWidgets.QListWidget):
+    def keyPressEvent(self, event):
+        super(EscapableQListWidget, self).keyPressEvent(event)
+        if event.key() == QtCore.Qt.Key_Escape:
+            self.clearSelection()
+
+
+class UniqueLabelQListWidget(EscapableQListWidget):
+    def mousePressEvent(self, event):
+        super(UniqueLabelQListWidget, self).mousePressEvent(event)
+        if not self.indexAt(event.pos()).isValid():
+            self.clearSelection()
+
+    def findItemsByLabel(self, label):
+        items = []
+        for row in range(self.count()):
+            item = self.item(row)
+            if item.data(QtCore.Qt.UserRole) == label:
+                items.append(item)
+        return items
+
+    def createItemFromLabel(self, label):
+        item = QtWidgets.QListWidgetItem()
+        item.setData(QtCore.Qt.UserRole, label)
+        return item
+
+    def setItemLabel(self, item, label, color=None):
+        qlabel = QtWidgets.QLabel()
+        if color is None:
+            qlabel.setText("{}".format(label))
+        else:
+            qlabel.setText(
+                '{} <font color="#{:02x}{:02x}{:02x}">●</font>'.format(
+                    label, *color
+                )
+            )
+        qlabel.setAlignment(QtCore.Qt.AlignBottom)
+
+        item.setSizeHint(qlabel.sizeHint())
+
+        self.setItemWidget(item, qlabel)
